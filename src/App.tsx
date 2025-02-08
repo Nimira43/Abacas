cb09fa6020972acff3ed117f56c83521861e92d6
Nimira43 <n.rabone72@aol.com>
2025-02-08 23:55:48 +0000

    Using Resource to define a resource and ListGuesser to generate a list view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import {
   Admin,
-  Resource,
   ListGuesser,
-  EditGuesser,
-  ShowGuesser,
+  Resource
 } from 'react-admin'
 import { Layout } from './Layout'
 import { dataProvider } from './dataProvider'
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}></Admin>
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource name='posts' list={ListGuesser} />
+  </Admin>
 )
--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ const router = createHashRouter([
   },
 ])
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
