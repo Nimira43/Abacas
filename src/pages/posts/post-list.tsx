6c3b8d83ec3630db09a504e55694f134f5a6e394
Nimira43 <n.rabone72@aol.com>
2025-02-09 00:43:32 +0000

    Formatting code in post-list.tsx

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -3,16 +3,19 @@ import { Datagrid, List, ReferenceField, TextField, FunctionField } from 'react-
 const PostList = () => (
     <List>
     <Datagrid>
-        
-        <TextField source='id' label='Post ID' />
-        <TextField source='title' label='Post Title' />
-        <FunctionField
-          label='Post Content'
-          render={(record) => `
-            ${record.body.substring(0, 50)}...
-          `}
-        />
-        <ReferenceField source='userId' reference='users' />
+      <TextField
+          source='id' label='ID' />
+      <TextField source='title' label='Post Title' />
+      <FunctionField
+        label='Post Content'
+        render={(record) => `
+          ${record.body.substring(0, 50)}...
+        `}
+      />
+      <ReferenceField
+        source='userId'
+        reference='users'
+      />
     </Datagrid>
   </List>
 )
