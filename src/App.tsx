461b65fdb05a01e1e2ac7a493f61c30ccf7631fd
Nimira43 <n.rabone72@aol.com>
2025-02-09 00:24:15 +0000

    Small update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import { dataProvider } from './dataProvider'
 import PostList from './pages/posts/post-list'
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}>
+  <Admin
+    layout={Layout}
+    dataProvider={dataProvider}
+  >
     <Resource name='posts' list={PostList} />
   </Admin>
 )
