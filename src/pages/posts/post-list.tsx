3d385c0440c7f2f5a453d14cad801baff840054a
Nimira43 <n.rabone72@aol.com>
2025-02-09 00:40:29 +0000

    ChAanging the labels of the fields and truncating the Post Content.

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,12 +1,17 @@
-import { Datagrid, List, ReferenceField, TextField } from 'react-admin'
+import { Datagrid, List, ReferenceField, TextField, FunctionField } from 'react-admin'
 
 const PostList = () => (
     <List>
     <Datagrid>
         
-        <TextField source='id' />
-        <TextField source='title' />
-        <TextField source='body' />
+        <TextField source='id' label='Post ID' />
+        <TextField source='title' label='Post Title' />
+        <FunctionField
+          label='Post Content'
+          render={(record) => `
+            ${record.body.substring(0, 50)}...
+          `}
+        />
         <ReferenceField source='userId' reference='users' />
     </Datagrid>
   </List>
