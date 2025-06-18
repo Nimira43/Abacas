a36256b110dd41ed8d69b64e6698250167ba85da
Nimira43 <n.rabone72@aol.com>
2025-06-18 10:09:20 +0100

    formData

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,19 +1,20 @@
 'use client'
 
 const AddTransaction = () => {
-  const clientAction = () => {
-
+  const clientAction = async(formData: FormData) => {
+    console.log(
+      formData.get('text'),
+      formData.get('amount')
+    )
   }
   
   return ( 
     <>
       <h3>Add Tranaction</h3>
-      <form 
-        action={() => {
-          clientAction()
-        }}
-      >
+      <form action={clientAction}>
+        <div className='formControl'>
 
+        </div>
       </form>
     </>
    )
