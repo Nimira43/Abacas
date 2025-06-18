a077ece69c6701128a5ccea55380a9f94a8ca367
Nimira43 <n.rabone72@aol.com>
2025-06-18 15:51:33 +0100

    AddTransaction - 2

--- app/actions/addTransaction.ts
+++ app/actions/addTransaction.ts
@@ -11,5 +11,12 @@ interface TransactionResult {
 }
 
 async function addTransaction(formData: FormData): Promise<TransactionResult> {
-  
+  const textValue = formData.get('text')
+  const amountValue = formData.get('amount')
+
+  if (!textValue || textValue === '' || !amountValue) {
+    return {
+      error: 'Text or amount is missing.'
+    }
+  }
 }
