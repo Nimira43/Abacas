30a00925ae75d236117689c58afb6db816fd760b
Nimira43 <n.rabone72@aol.com>
2025-06-18 08:29:51 +0100

    Creating checkUser

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,3 +1,12 @@
 import { currentUser } from '@clerk/nextjs/server'
 import { db } from './db'
 
+export const checkUser = async () => {
+  return ( 
+    const user = await currentUser()
+
+    
+  )
+}
+ 
+export default 
