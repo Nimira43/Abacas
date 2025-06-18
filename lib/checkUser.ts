12730068cc446972feca383f6874ac610d4178e4
Nimira43 <n.rabone72@aol.com>
2025-06-18 08:31:00 +0100

    Check for current user

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -2,10 +2,16 @@ import { currentUser } from '@clerk/nextjs/server'
 import { db } from './db'
 
 export const checkUser = async () => {
-  return ( 
-    const user = await currentUser()
+  const user = await currentUser()
+  
+  if (!user) {
+    return null
+  }
 
+  return ( 
     
+
+
   )
 }
  
