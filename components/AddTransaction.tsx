9e4804e660997949309b6432b8dca6a207d9c285
Nimira43 <n.rabone72@aol.com>
2025-06-18 10:04:57 +0100

    Creating clientAction function for form

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,7 +1,20 @@
+'use client'
+
 const AddTransaction = () => {
+  const clientAction = () => {
+
+  }
+  
   return ( 
     <>
       <h3>Add Tranaction</h3>
+      <form 
+        action={() => {
+          clientAction()
+        }}
+      >
+
+      </form>
     </>
    )
 }
