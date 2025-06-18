623055dfd8b831c5517686c51286c3d378f59653
Nimira43 <n.rabone72@aol.com>
2025-06-18 10:17:00 +0100

    Added labels and inputs.

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -13,7 +13,23 @@ const AddTransaction = () => {
       <h3>Add Tranaction</h3>
       <form action={clientAction}>
         <div className='formControl'>
-
+          <label htmlFor='text'>Text</label>
+          <input 
+            type='text' 
+            id='text' 
+            name='text' 
+            placeholder='Enter text...' 
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='amount'>Amount </label>
+          <input 
+            type='number' 
+            id='amount' 
+            name='amount' 
+            placeholder='Enter mount...' 
+            step='0.01'
+          />
         </div>
       </form>
     </>
