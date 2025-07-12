36a74eafc064782fcddd058f776287955fed96ba
Nimira43 <n.rabone72@aol.com>
2025-07-12 21:09:58 +0100

    To get balance - needs destructuring

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,12 +1,12 @@
 import getUserBalance from '@/app/actions/getUserBalance'
 
 const Balance = async () => {
-  const balance = await getUserBalance()
+  const { balance } = await getUserBalance()
 
   return (  
     <>
       <h4>Your Balance</h4>
-      <h1>£400</h1>
+      <h1>£{balance ?? 0}</h1>
     </>
   )
 }
