4476b3e4ebae495ebd9445cbd29c405e2c8f2b2d
Nimira43 <n.rabone72@aol.com>
2025-08-13 09:25:31 +0100

    TransactionList -2

--- components/transactionList.tsx
+++ components/transactionList.tsx
@@ -3,7 +3,13 @@ const TransactionList = () => {
     <>
       <h3>Previous Transactions</h3>
       <ul className='list'>
-        
+        {transactions && 
+          transactions.map(( 
+            transaction: Transaction) => (
+              <p>{ transaction.text}</p>
+            )
+          )
+        }
       </ul>
     </>
    )
