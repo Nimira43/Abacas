39e1e726d6cebafa60ee040c633cdc1c454a6694
Nimira43 <n.rabone72@aol.com>
2025-08-13 09:19:24 +0100

    TransactionList -1

---
+++ components/transactionList.tsx
@@ -0,0 +1,12 @@
+const TransactionList = () => {
+  return ( 
+    <>
+      <h3>Previous Transactions</h3>
+      <ul className='list'>
+        
+      </ul>
+    </>
+   )
+}
+ 
+export default TransactionList
